f11e687ce723cbc352609306fbc967933c70d118
anastasiakr <anastasiia.lipatova@gmail.com>
2022-11-08 11:34:55 -0600

    clean-up poverty_script.py

--- poverty_script.py
+++ poverty_script.py
@@ -1,4 +1,4 @@
-""" Get data from the URL resource and save it to CSV file"""
+""" Get poverty data from the URL resource and save it to CSV file"""
 
 import csv
 import logging
@@ -7,45 +7,41 @@ import requests
 from decouple import config
 from requests.auth import HTTPBasicAuth
 
-
 logging.basicConfig(level=logging.INFO)
+
 # URL to the data resource
 URL = "https://api.census.gov/data/timeseries/poverty/saipe"
+
+# Set up auth if it's required. Edit API KEY and SECRET in your .env file
+AUTH = HTTPBasicAuth(config("API_KEY"), config("SECRET"))
 
 # specify required data parameters
 STATE_CODE = 48  # Texas
 COUNTRY_CODE = 201  # Harris County
 YEAR = 2018
 
-indicators = ["NAME",  # county name
-              "SAEPOV0_17_PT",  # Ages 0-17 in Poverty, Count Estimate
-              "SAEPOVALL_PT",  # All ages in Poverty, Count Estimate
-              "SAEMHI_PT",  # Median Household Income Estimate
-              ]
-
-# specify CSV header and filename to save data
-csv_header = [
-    "County name",
-    "Estimate poverty under 18",
-    "Estimate poverty any age",
-    "Estimate median household income",
-    "Year",
-    "State code",
-    "County code",
+INDICATORS = [
+    "SAEPOV0_17_PT",  # Ages 0-17 in Poverty, Count Estimate
+    "SAEPOVALL_PT",  # All ages in Poverty, Count Estimate
+    "SAEMHI_PT",  # Median Household Income Estimate
 ]
-
-CSV_FILENAME = "poverty_2018_Harris_TX_.csv"
 
 # build URL params
 url_params = {
-    "get": ",".join(indicators),
+    "get": ",".join(INDICATORS),
     "for": f"county:{COUNTRY_CODE}",
     "in": f"state:{STATE_CODE}",
     "time": YEAR,
 }
 
-# Set up auth if it's required. Edit API KEY and SECRET in your .env file
-AUTH = HTTPBasicAuth(config("API_KEY"), config("SECRET"))
+# specify CSV header and filename to save data
+CSV_HEADER = [
+    "Estimate poverty under 18",
+    "Estimate poverty any age",
+    "Estimate median household income",
+]
+
+CSV_FILENAME = "poverty_2018_Harris_TX_.csv"
 
 
 def get_data(url: str, params: dict, auth: requests.auth.HTTPBasicAuth) -> list:
@@ -57,7 +53,8 @@ def get_data(url: str, params: dict, auth: requests.auth.HTTPBasicAuth) -> list:
     response.raise_for_status()
     json_data = response.json()
     logging.info("JSON data received")
-    return json_data[1]
+    # remove extra data when returning json
+    return json_data[1][0:3]
 
 
 def save_to_csv(header: list, csv_filename: str, json_data: list) -> None:
@@ -76,4 +73,4 @@ if __name__ == "__main__":
     data = get_data(URL, url_params, AUTH)
 
     # save data to CSV
-    save_to_csv(csv_header, CSV_FILENAME, data)
+    save_to_csv(CSV_HEADER, CSV_FILENAME, data)
